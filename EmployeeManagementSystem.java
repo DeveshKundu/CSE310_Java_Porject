7c69a569ad944fa6a344a609100f66ea1dc80d16
vivekkumar45 <92422766+vivekkumar45@users.noreply.github.com>
2023-04-13 22:32:29 +0530

    Update EmployeeManagementSystem.java

--- EmployeeManagementSystem.java
+++ EmployeeManagementSystem.java
@@ -13,6 +13,54 @@ class Employee {
     private double salary;
     private double taxRate;
     private double niRate;
+    
+public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public double getTaxRate() {
+        return taxRate;
+    }
+
+    public void setTaxRate(double taxRate) {
+        this.taxRate = taxRate;
+    }
+
+    public double getNiRate() {
+        return niRate;
+    }
+
+    public void setNiRate(double niRate) {
+        this.niRate = niRate;
+    }
+
+    public double getGrossPay() {
+        return salary;
+    }
+
+    public double getTax() {
+        return salary * taxRate;
+    }
+
+    public double getNationalInsurance() {
+        return salary * niRate;
+    }
+
+    public double getNetPay() {
+        return salary - getTax() - getNationalInsurance();
+    }
 
 public class EmployeeManagementSystem {
     public static void main(String[] args) {
